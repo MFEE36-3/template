db5d1230aa6a5a9640e9d39bee0c92c87329f9ee
signnn <sign1113@ymail.com.tw>
2023-05-11 10:19:39 +0800

    test1

---
+++ test_page_message.php
@@ -0,0 +1,11 @@
+<?php include "./backend_header.php" ?>
+<?php include "./backend_navbar_and_sidebar.php" ?>
+
+<div class="w-100 p-3 mb-auto">
+    <div class="container-fluid w-100"> <!--這個的class可以自己改掉，給你們看範圍的而已-->
+        你們的東123543545面
+    </div>
+</div>
+
+<?php include "./backend_footer.php" ?>
+<?php include "./backend_js_and_endtag.php" ?>
