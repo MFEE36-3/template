041a170c0a93a497b6c5b2b416b30439bca3603a
kaikat0211 <kaifat7887@gmail.com>
2023-05-15 00:05:20 +0800

    0515

--- test_page.php
+++ test_page.php
@@ -7,8 +7,8 @@
             <div class="col mt-3 d-flex ">
                 <div class="input-group mb-3 w-50">
                     <input type="text" class="form-control searchradius1 fs-4"
-                        aria-label="Amount (to the nearest dollar)" placeholder="Search item">
-                    <span class="input-group-text searchradius2"><i class="fa-solid fa-magnifying-glass"></i></span>
+                        aria-label="Amount (to the nearest dollar)" placeholder="Search item" id="searchItem">
+                    <span class="input-group-text searchradius2 pointer" onclick="search()"><i class="fa-solid fa-magnifying-glass " ></i></span>
                 </div>
                 <div class="ms-4 d-flex align-items-center mb-3" id="shopcategory">
                     <div class="me-4">
@@ -65,13 +65,13 @@
 
                     </tbody>
                 </table>
-                <nav aria-label="..." class="d-flex justify-content-center mt-3 page">
-                    <ul class="pagination pagination-lg">
+                <nav aria-label="..." class="d-flex justify-content-center mt-3">
+                    <ul class="pagination pagination-lg ">
                         <li class="page-item active" aria-current="page">
                             <span class="page-link">1</span>
                         </li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="http://wuchengrudemacbook-air.local/template/test_page.php#page=2&totalshow=6">2</a></li>
+                        <li class="page-item"><a class="page-link" href="http://wuchengrudemacbook-air.local/template/test_page.php#page=3&totalshow=6">3</a></li>
                     </ul>
                 </nav>
             </div>
@@ -84,44 +84,46 @@
             </div>
         </div>
         <form action="./php_form.php" method="post" onsubmit="submitForm()" id="addform" class="d-none">
-            <div class="position-fixed top-50 start-50 translate-middle formbr" style="background-color: #f0f0f0;">
+            <div class="position-fixed top-50 start-50 translate-middle formbr" style="background-color: #E1F5FE;">
                 <div class="inputadd pt-4">
                     <div class="mx-auto fs-2 mb-3">
                         <div>新增商品</div>
                     </div>
                     <div class="ms-3">
                         <div class="innerinput mb-3">
-                            <label for="itemName">商品名稱:</label>
-                            <input type="text" name="itemName" id="itemName" style="background-color: #fe7a7b"
+                            <label for="itemName" style="color:#555555">商品名稱:</label>
+                            <input type="text" name="itemName" id="itemName" style="background-color: #1976D2"
                                 class="inputborder">
                         </div>
                         <div class="innerinput mb-3">
-                            <label for="cate">商品類別:</label>
-                            <input type="text" name="cate" id="cate" style="background-color: #fe7a7b"
+                            <label for="cate" style="color:#555555">商品類別:</label>
+                            <input type="text" name="cate" id="cate" style="background-color: #1976D2"
                                 class="inputborder">
                         </div>
                         <div class="innerinput mb-3">
-                            <label for="imgSrc">商品圖片:</label>
+                            <label for="imgSrc" style="color:#555555">商品圖片:</label>
                             <input type="file" name="imgSrc" id="imgSrc">
                         </div>
                         <div class="innerinput mb-3">
-                            <label for="price">商品價格:</label>
-                            <input type="text" name="price" id="price" style="background-color: #fe7a7b"
+                            <label for="price" style="color:#555555">商品價格:</label>
+                            <input type="text" name="price" id="price" style="background-color: #1976D2"
                                 class="inputborder">
                         </div>
                         <div class="innerinput mb-3">
-                            <label for="stock">商品庫存:</label>
-                            <input type="text" name="stock" id="stock" style="background-color: #fe7a7b"
+                            <label for="stock" style="color:#555555">商品庫存:</label>
+                            <input type="text" name="stock" id="stock" style="background-color: #1976D2"
                                 class="inputborder">
                         </div>
                         <div class="descrip d-flex">
-                            <label for="description">商品敘述:</label>
+                            <label for="description" style="color:#555555">商品敘述:</label>
                             <textarea name="description" id="description" cols="30" rows="7"
-                                style="background-color: #fe7a7b" class="inputborder ms-2"></textarea>
+                                style="background-color: #1976D2" class="inputborder ms-2"></textarea>
                         </div>
                         <div class="addbtngroup my-3">
-                            <button type="submit" class="addbtnstyle p-2 rounded-3 pointer">送出</button>
-                            <button type="button" class="addbtnstyle p-2 rounded-3 pointer" id="closeAdd">取消</button>
+                            <button type="submit" class="addbtnstyle p-2 rounded-3 pointer"
+                                style="background-color: #ffb74d">送出</button>
+                            <button type="button" class="addbtnstyle p-2 rounded-3 pointer" id="closeAdd"
+                                style="background-color: #D2E3F7">取消</button>
                         </div>
                     </div>
                 </div>
@@ -129,8 +131,14 @@
 
         </form>
         <script>
+            //searchbox start
+            let searchItem =document.getElementById("searchItem");
+            let search = ()=>{
+                console.log(searchItem.value);
+            }
+            //searchbox end
             //test data start
-            let testData = [
+            let testPushData = [
                 {
                     "itemId": "1",
                     "cate": "freshfood",
@@ -150,9 +158,140 @@
                     "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
                     "stock": "40",
                     "date": "2023-03-23"
+                },
+                {
+                    "itemId": "3",
+                    "cate": "freshfood",
+                    "itemName": "香蕉",
+                    "imgSrc": ".\/kaiimgs\/freshfood3.jpeg",
+                    "price": "50",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "40",
+                    "date": "2023-03-23"
+                },
+                {
+                    "itemId": "4",
+                    "cate": "freshfood",
+                    "itemName": "香蕉",
+                    "imgSrc": ".\/kaiimgs\/freshfood3.jpeg",
+                    "price": "50",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "40",
+                    "date": "2023-03-23"
+                },
+                {
+                    "itemId": "5",
+                    "cate": "freshfood",
+                    "itemName": "香蕉",
+                    "imgSrc": ".\/kaiimgs\/freshfood3.jpeg",
+                    "price": "50",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "40",
+                    "date": "2023-03-23"
+                },
+                {
+                    "itemId": "6",
+                    "cate": "freshfood",
+                    "itemName": "香蕉",
+                    "imgSrc": ".\/kaiimgs\/freshfood3.jpeg",
+                    "price": "50",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "40",
+                    "date": "2023-03-23"
+                },
+                {
+                    "itemId": "7",
+                    "cate": "freshfood",
+                    "itemName": "香蕉",
+                    "imgSrc": ".\/kaiimgs\/freshfood3.jpeg",
+                    "price": "50",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "40",
+                    "date": "2023-03-23"
+                },
+                {
+                    "itemId": "8",
+                    "cate": "freshfood",
+                    "itemName": "香蕉",
+                    "imgSrc": ".\/kaiimgs\/freshfood3.jpeg",
+                    "price": "50",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "40",
+                    "date": "2023-03-23"
                 }
             ];
-
+            let testReadyData = [
+                {
+                    "itemId": "1",
+                    "cate": "drink",
+                    "itemName": "飲料",
+                    "imgSrc": ".\/kaiimgs\/drink.jpeg",
+                    "price": "80",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "90",
+                    "date": "2023-03-03"
+                },
+                {
+                    "itemId": "2",
+                    "cate": "drink",
+                    "itemName": "飲料",
+                    "imgSrc": ".\/kaiimgs\/drink.jpeg",
+                    "price": "80",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "90",
+                    "date": "2023-03-03"
+                },
+                {
+                    "itemId": "3",
+                    "cate": "drink",
+                    "itemName": "飲料",
+                    "imgSrc": ".\/kaiimgs\/drink.jpeg",
+                    "price": "80",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "90",
+                    "date": "2023-03-03"
+                },
+                {
+                    "itemId": "4",
+                    "cate": "drink",
+                    "itemName": "飲料",
+                    "imgSrc": ".\/kaiimgs\/drink.jpeg",
+                    "price": "80",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "90",
+                    "date": "2023-03-03"
+                },
+                {
+                    "itemId": "5",
+                    "cate": "drink",
+                    "itemName": "飲料",
+                    "imgSrc": ".\/kaiimgs\/drink.jpeg",
+                    "price": "80",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "90",
+                    "date": "2023-03-03"
+                },
+                {
+                    "itemId": "6",
+                    "cate": "drink",
+                    "itemName": "飲料",
+                    "imgSrc": ".\/kaiimgs\/drink.jpeg",
+                    "price": "80",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "90",
+                    "date": "2023-03-03"
+                },
+                {
+                    "itemId": "7",
+                    "cate": "drink",
+                    "itemName": "飲料",
+                    "imgSrc": ".\/kaiimgs\/drink.jpeg",
+                    "price": "80",
+                    "description": "採用有機農場生產之香蕉，有機栽種、高度營養價值，健康無負擔，讓您吃得安心又滿足。",
+                    "stock": "90",
+                    "date": "2023-03-03"
+                },
+            ];
             //test data end
             //control innertable start
             let divList = document.querySelectorAll(".searching");
@@ -186,9 +325,9 @@
             }
             document.addEventListener("DOMContentLoaded", () => {
                 divList[0].classList.add("selected", "bg-warning");
-                testData.forEach((data => {
+                testPushData.forEach((data => {
                     let { itemId, cate, itemName, imgSrc, price, description, stock, date } = data;
-                    tBody.innerHTML = `
+                    tBody.innerHTML += `
                         <tr class="text-center tritem">
                             <td class="py-4">
                                 <img src=${imgSrc} class="photofix">
@@ -209,6 +348,7 @@
                             </td>
                         </tr>
                         `
+
                 }))
             })
             let pushItem = document.getElementById("pushItem");
@@ -217,8 +357,14 @@
             let tHead = document.getElementById("tHead");
             let tBody = document.getElementById("tBody");
             pushItem.addEventListener("click", () => {
-                testData.forEach((data => {
+                tHead.innerHTML = "";
+                tBody.innerHTML = "";
+                if(testPushData.length > 6){
+                    testPushData.filter
+                }
+                testPushData.forEach((data => {
                     let { itemId, cate, itemName, imgSrc, price, description, stock, date } = data;
+
                     tHead.innerHTML =
                         `<tr>
                         <td class="py-3">圖片</td>
@@ -230,7 +376,7 @@
                         <td class="py-3">上架日期</td>
                         <td class="py-3">編輯</td>
                     </tr>`
-                    tBody.innerHTML = `
+                    tBody.innerHTML += `
                         <tr class="text-center tritem">
                             <td class="py-4">
                                 <img src=${imgSrc} class="photofix">
@@ -254,8 +400,12 @@
                 }))
             })
             readyPushItem.addEventListener("click", () => {
-                tHead.innerHTML =
-                    `<tr>
+                tHead.innerHTML = "";
+                tBody.innerHTML = "";
+                testReadyData.forEach((data => {
+                    let { itemId, cate, itemName, imgSrc, price, description, stock, date } = data;
+                    tHead.innerHTML =
+                        `<tr>
                         <td class="py-3">圖片</td>
                         <td class="py-3">商品名稱</td>
                         <td class="py-3">商品類別</td>
@@ -265,35 +415,28 @@
                         <td class="py-3">上架日期</td>
                         <td class="py-3">編輯</td>
                     </tr>`
-                tBody.innerHTML = `<tr class="text-center">
+                    tBody.innerHTML += `
+                        <tr class="text-center tritem">
                             <td class="py-4">
-                                <img src="./kaiimgs/img1.jpeg" class="photofix">
+                                <img src=${imgSrc} class="photofix">
                             </td>
                             <td>
-                                風景圖
+                                ${itemName}
                             </td>
                             <td>
-                                照片
+                                ${cate}
                             </td>
-                            <td>海邊的照片片片片片片片片片片</td>
-                            <td>90</td>
-                            <td>100</td>
-                            <td>2023-05-11</td>
+                            <td>${description}</td>
+                            <td>${price}</td>
+                            <td>${stock}</td>
+                            <td>${date}</td>
                             <td class="fs-3">
                                 <i class="fa-solid fa-pencil pointer" onclick="edit()"></i>
-                                <i class="fa-solid fa-delete-left pointer ms-1" onclick="remove()"></i>
+                                <i class="fa-solid fa-delete-left pointer ms-1" id="remove-${itemId}" onclick="remove(this)"></i>
                             </td>
                         </tr>
-                        <tr class="text-center">
-                            <td class="py-4">
-                                <img src="./kaiimgs/drink.jpeg" class="photofix">
-                            </td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>`
+                        `
+                }))
             })
             wishItem.addEventListener("click", () => {
                 tHead.innerHTML =
