f4797b6e3eb020d978c9ad1c73b46c9c37c650b6
Daidai <sc931612@gmail.com>
2023-05-16 00:15:14 +0800

    adjust login & add login_api

--- login.php
+++ login.php
@@ -79,7 +79,7 @@
     }
 
     #form1 {
-        background-color: rgba(255, 255, 255, 0.5);
+        background-color: rgba(255, 255, 255, 0.4);
         border-radius: 15px;
     }
 
@@ -90,15 +90,19 @@
         border-radius: 5px;
         border: none;
         padding: 5px 15px;
+        border: 5px solid gold;
+        transition: 3s ease-in-out;
+        color: white;
+        font-weight: bold;
     }
 
-    input:focus {
+    #form1 input:focus {
         width: 200px;
         height: 30px;
         box-sizing: border-box;
         border: none;
         outline: none;
-        border: 2px solid lightcoral;
+        background-color: tomato;
     }
 
     .btn-primary {
@@ -107,7 +111,63 @@
     }
 
     .btn-primary:hover {
-        transform: scale(1.1);
+        transform: scale(1.5);
+        border-radius: 50px;
+        font-weight: 900;
+    }
+
+    #show_input {
+        height: 0px;
+        overflow: hidden;
+        transition: 1s ease-in-out;
+    }
+
+    label {
+        color: tomato;
+        transition: 3s ease-in-out;
+    }
+
+    #p1 {
+        background-color: lightgoldenrodyellow;
+        border-radius: 15px;
+        width: 150px;
+    }
+
+    #myDialog::backdrop {
+        background-color: rgba(0, 0, 0, 0.6);
+    }
+
+    #myDialog {
+        margin: auto;
+        text-align: center;
+        width: 300px;
+        height: 300px;
+        background-color: rgb(1, 180, 104);
+        border-radius: 50%;
+        border: none;
+        /* border: 15px 15px white;
+        box-shadow:
+            2px 2px 5px black, */
+    }
+
+    #badDialog::backdrop {
+        background-color: rgba(0, 0, 0, 0.6);
+    }
+
+    #badDialog {
+        margin: auto;
+        width: 300px;
+        height: 300px;
+        background-color: rgb(255, 81, 5);
+        border-radius: 50%;
+        border: none;
+        /* border: 5px 5px white;
+        box-shadow:
+            2px 2px 5px black, */
+    }
+
+    .p_login {
+        font-size: 80px;
     }
 </style>
 
@@ -120,14 +180,30 @@
             <span class="text-danger move3" style="font-family: 'Source Code Pro', monospace;">EAT!</span>
         </div>
         <button class="btn btn-primary" id="login" style="width:100px">登入</button>
-        <div id="show_input" style="display:none">
-            <form action="" id="form1" class="d-flex flex-column p-5">
+        <div id="show_input">
+            <form id="form1" class="d-flex flex-column p-5" name="form1" onsubmit="check_password(event)">
                 <label for="email" class="fw-bold ms-2 mb-1" style="font-family: 'Noto Sans JP', sans-serif;">帳號</label>
                 <input type="text" id="email" name="email">
+                <p id="p1" class="text-danger fw-bold text-center mt-2 ms-4"></p>
                 <label for="password" class="fw-bold ms-2 mb-1 mt-2" style="font-family: 'Noto Sans JP', sans-serif;">密碼</label>
                 <input type="password" id="password" name="password">
+                <button class="btn btn-danger mt-3 text-center fw-bold" id="check" style="width:100px">GO!</button>
             </form>
         </div>
+        <dialog id="myDialog">
+            <div class="d-flex flex-column justify-content-center align-items-center mt-4">
+                <div class="" style="width:200px">
+                    <p class="text-white fw-bold text-center p_login m-0">登入成功</p>
+                </div>
+            </div>
+        </dialog>
+        <dialog id="badDialog">
+            <div class="d-flex flex-column justify-content-center align-items-center mt-4">
+                <div class="" style="width:200px">
+                    <p class="text-white fw-bold text-center p_login m-0">登入失敗</p>
+                </div>
+            </div>
+        </dialog>
     </div>
 
 
@@ -137,10 +213,66 @@
 
     <script>
         const btn1 = document.getElementById('login');
+        const labels = document.querySelectorAll('label');
+        const inputs = document.querySelectorAll('input');
+        const email_input = document.getElementById('email');
+        const email_check = /^[^@\.]+@([^@\.]+\.)+[^@\.]{2,3}$/;
+        const p1 = document.getElementById('p1');
+
         btn1.addEventListener('click', () => {
             btn1.style.display = "none";
             show_input.style.display = "block";
+            show_input.style.height = "300px";
+            labels[0].style.color = "black";
+            labels[1].style.color = "black";
+            inputs[0].style.border = "2px solid transparent";
+            inputs[1].style.border = "2px solid transparent";
         })
+
+        let ispass = true;
+
+        email_input.addEventListener('change', () => {
+            if (email_check.test(email_input.value) == true) {
+                p1.innerHTML = `帳號格式正確`;
+            } else {
+                p1.innerHTML = `請檢查帳號格式`;
+                ispass = false;
+            }
+            setTimeout(() => {
+                p1.innerHTML = "";
+            }, 2000);
+        })
+
+        function check_password(event) {
+            event.preventDefault();
+
+            if (ispass) {
+                const fd = new FormData(document.form1);
+
+                fetch('login_api.php', {
+                        method: 'POST',
+                        body: fd //可以省略Content-type  multipart form/data
+                    })
+                    .then(r => r.json())
+                    .then(obj => {
+                        console.log(obj);
+                        if (obj.success) {
+                            myDialog.showModal();
+                            setTimeout(() => {
+                                location.href = 'dai_coupon_page.php';
+                            }, 2000);
+                        } else {
+                            badDialog.showModal();
+                            setTimeout(() => {
+                                badDialog.close()
+                            }, 2000);
+                        }
+
+                    })
+
+            };
+
+        }
     </script>
 
 
