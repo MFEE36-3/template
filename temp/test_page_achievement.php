a67c53111e198ddd9fdd091a58c44bd80ede7d2d
luke1128qq <ggeniussquid@gmail.com>
2023-05-16 10:34:11 +0800

    six

---
+++ temp/test_page_achievement.php
@@ -0,0 +1,107 @@
+<?php
+require '../m_path/connect.php';
+
+$perPage = 10; # 每頁最多幾筆
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1; # 用戶要看第幾頁
+
+if ($page < 1) {
+    header('Location: ?page=1');
+    exit;
+}
+
+$t_sql = "SELECT COUNT(1) FROM achievement";
+$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0]; # 總筆數
+$totalPages = ceil($totalRows / $perPage); # 總頁數
+$rows = [];
+
+if ($totalRows) {
+    if ($page > $totalPages) {
+        header("Location: ?page=$totalPages");
+        exit;
+    }
+    $sql = sprintf("SELECT * FROM achievement ORDER BY sid ASC LIMIT %s, %s", ($page - 1) * $perPage, $perPage);
+
+    $rows = $pdo->query($sql)->fetchAll();
+}
+
+?>
+
+<?php include "./backend_header.php" ?>
+<?php include "./backend_navbar_and_sidebar.php" ?>
+
+<div class="w-100 p-3 mb-auto overflow-scroll" style="height: 700px;">
+    <div class="container m-3"> <!--這個的class可以自己改掉，給你們看範圍的而已-->
+        <div class="row mt-4" style="height: 70px;">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item <?= 1 == $page ? 'disabled' : '' ?>">
+                        <a class="page-link" href="?page=1">
+                            <i class="fa-solid fa-angles-left"></i>
+                        </a>
+                    </li>
+                    <li class="page-item <?= 1 == $page ? 'disabled' : '' ?>">
+                        <a class="page-link" href="?page=<?= $page - 1 ?>">
+                            <i class="fa-solid fa-angle-left"></i>
+                        </a>
+                    </li>
+                    <?php for ($i = $page - 4; $i <= $page + 4; $i++) :
+                        if ($i >= 1 and $i <= $totalPages) :
+                    ?>
+                            <li class="page-item <?= $i == $page ? 'active' : '' ?>">
+                                <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                            </li>
+                    <?php endif;
+                    endfor; ?>
+                    <li class="page-item <?= $totalPages == $page ? 'disabled' : '' ?>">
+                        <a class="page-link" href="?page=<?= $page + 1 ?>">
+                            <i class="fa-solid fa-angle-right"></i>
+                        </a>
+                    </li>
+                    <li class="page-item <?= $totalPages == $page ? 'disabled' : '' ?>">
+                        <a class="page-link" href="?page=<?= $totalPages ?>">
+                            <i class="fa-solid fa-angles-right"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="row">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th style="text-align: center; width: 20px" scope="col">成就編號</th>
+                        <th style="text-align: center; width: 70px" scope="col">成就名稱</th>
+                        <th style="text-align: center; width: 150px" scope="col">如何取得成就</th>
+                        <th style="text-align: center; width: 200px" scope="col">獎勵徽章</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($rows as $r) : ?>
+                        <tr>
+                            <td style="text-align: center; width: 20px"><?= $r['sid'] ?></td>
+                            <td style="text-align: center; width: 70px"><?= $r['name'] ?></td>
+                            <td style="text-align: center; width: 150px"><?= $r['how_to_get'] ?></td>
+                            <td style="text-align: center; width: 200px"><?= $r['badge'] ?></td>
+
+                        </tr>
+                    <?php endforeach; ?>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <?php include "./backend_footer.php" ?>
+
+    <script>
+        document.querySelector('li.page-item.active a').removeAttribute('href');
+
+        function delete_it(sid) {
+            if (confirm(`是否要刪除編號為 ${sid} 的資料?`)) {
+                location.href = 'delete.php?sid=' + sid;
+            }
+
+        }
+    </script>
+
+    <?php include "./backend_js_and_endtag.php" ?>
---
+++ temp/test_page_delete_api.php
@@ -0,0 +1,29 @@
+<?php
+require '../m_path/connect.php';
+
+if (isset($_GET['sid'])) {
+    $sid = intval($_GET['sid']);
+
+    // 從資料庫讀取會員資料
+    $stmt = $pdo->prepare("SELECT * FROM member_info WHERE sid = ?");
+    $stmt->execute([$sid]);
+    $r = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$r) {
+        // 找不到該會員資料
+        header('Location: test_page_member_info.php');
+        exit;
+    }
+
+    // 執行刪除操作
+    $stmt = $pdo->prepare("DELETE FROM member_info WHERE sid = ?");
+    $stmt->execute([$sid]);
+
+    // 刪除成功，轉到會員列表頁面
+    header('Location: test_page_member_info.php');
+    exit;
+} else {
+    // 沒有提供會員編號，轉到會員列表頁面
+    header('Location: test_page_member_info.php');
+    exit;
+}
