ec2a4f7aee85cc84e4a3c08049b0b5796d31e7a3
kaikat0211 <kaifat7887@gmail.com>
2023-05-18 16:27:13 +0800

    update sidebar

--- backend_navbar_and_sidebar.php
+++ backend_navbar_and_sidebar.php
@@ -131,22 +131,15 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                    <i class="icon-layout menu-icon"></i>
-                    <span class="menu-title">UI Elements</span>
+                    <i class="icon-layout menu-icon fa-brands fa-shopify"></i>
+                    <span class="menu-title">商城管理系統</span>
                     <i class="menu-arrow"></i>
                 </a>
                 <div class="collapse" id="ui-basic">
                     <ul class="nav flex-column sub-menu">
                         <li class="nav-item">
-                            <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a>
+                            <a class="nav-link" href="http://localhost/template/kaishop.php">商品管理</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="pages/ui-features/typography.html">Typography</a>
-                        </li>
-                    </ul>
                 </div>
             </li>
             <li class="nav-item">
